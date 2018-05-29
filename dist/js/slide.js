d5bc48dd47a02cdfd1988689a1aab6c98c4a6b59
Unknown <okokis101@gmail.com>
2018-05-30 02:27:54 +0800

    Merge branch 'feature/header' into develop
    and reclear code

---
+++ dist/js/slide.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        margin: 10,
+        nav: true,
+        center: true,
+        items: 2
+    })
+});
