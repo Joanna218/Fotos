cb4dad7eec3fb8ef26d1614433278b82406e4213
Joanna Lin <okokis101@gmail.com>
2018-06-02 19:01:04 +0800

    add aos & animate

--- dist/js/slide.js
+++ dist/js/slide.js
@@ -1,4 +1,16 @@
 $(function() {
+
+    var window_height = $(window).height();
+    var row3_height = window_height - 148;
+    $('.img_header').css({
+        height: window_height
+    });
+    $('._container_row3').css({
+        height: row3_height
+    });
+
+    ///////////////////////////////////////////
+
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
@@ -17,4 +29,14 @@ $(function() {
             }
         }
     })
+
+
+    //////////////////////////////////////////////////
+
+    AOS.init({
+        offset: 100,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 100,
+    });
 })
