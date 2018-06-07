be77d65f4875c850b84865ac5cea3015d794b446
Joanna Lin <okokis101@gmail.com>
2018-06-08 04:18:51 +0800

    add home_ad & buy_shop

---
+++ dist/js/buy_shop.js
@@ -0,0 +1,34 @@
+$(function() {
+    $('.plus1').click(function() {
+        $('.info1').slideToggle()
+    });
+    $('.plus2').click(function() {
+        $('.info2').slideToggle()
+    });
+    $('.plus3').click(function() {
+        $('.info3').slideToggle()
+    });
+    ///////////////////////////////////////////////
+    //隱藏tab1以外的內容
+    $('.contents div[id != "tab1"]').hide();
+
+    // 點選頁籤
+    $("a").click(function() {
+        // 先將所有內容設為隱藏不顯示
+        $(".contents div").hide();
+
+        // 顯示出選擇頁籤的對應內容
+        var res = $(this).attr("href");
+        console.log(res);
+        $(res).show();
+
+        // 移除目前的current類別
+        $(".current").removeClass("current");
+
+        // 在目前所選頁籤本身加上current類別
+        $(this).addClass("current");
+
+        return false;
+    });
+
+});
