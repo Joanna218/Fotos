510862f8842cc4a6dc28ff7f0e020340474b4585
Joanna Lin <okokis101@gmail.com>
2018-06-10 03:26:02 +0800

    complete buy_shop 70%

--- dist/js/buy_shop.js
+++ dist/js/buy_shop.js
@@ -1,34 +1,111 @@
-$(function() {
-    $('.plus1').click(function() {
-        $('.info1').slideToggle()
-    });
-    $('.plus2').click(function() {
-        $('.info2').slideToggle()
-    });
-    $('.plus3').click(function() {
-        $('.info3').slideToggle()
-    });
-    ///////////////////////////////////////////////
-    //隱藏tab1以外的內容
-    $('.contents div[id != "tab1"]').hide();
+ let flag = 0;
+ $(function() {
+     // 數量計數器
+     //要關上
+     $('.plus1').on('click', function() {
+         if (flag == 0) {
+             $('.info1').slideUp(function() {
+                 $('.plus1').replaceWith('<div class="plus1">-</div>');
+                 flag = 1;
+                 console.log(flag);
+             })
+         } else if (flag == 1) {
+             //flag = 1; // 打開
+             $('.info1').slideDown(function() {
+                 $('.plus1').replaceWith('<div class="plus1">+</div>');
+                 flag = 0;
+                 console.log(flag);
+             })
+         }
+     });
+     $('.plus2').click(function() {
+         $('.info2').slideToggle()
+     });
+     $('.plus3').click(function() {
+         $('.info3').slideToggle()
+     });
 
-    // 點選頁籤
-    $("a").click(function() {
-        // 先將所有內容設為隱藏不顯示
-        $(".contents div").hide();
 
-        // 顯示出選擇頁籤的對應內容
-        var res = $(this).attr("href");
-        console.log(res);
-        $(res).show();
+     //......................................
+     //商品評論頁籤
+     $('.contents div[id != "tab1"]').hide();
+     $("a").click(function() {
+         $(".contents div").hide();
 
-        // 移除目前的current類別
-        $(".current").removeClass("current");
+         // 顯示出選擇頁籤的對應內容
+         var res = $(this).attr("href");
+         console.log(res);
+         $(res).show();
 
-        // 在目前所選頁籤本身加上current類別
-        $(this).addClass("current");
+         // 移除目前的current類別
+         $(".current").removeClass("current");
 
-        return false;
-    });
+         // 在目前所選頁籤本身加上current類別
+         $(this).addClass("current");
 
-});
+         return false;
+     });
+
+     //..................................
+     //計數器加減數量
+     $(".minus").on('click', function() {
+         var value = $(".num").val();
+         value--;
+         if (value < 2) {
+             var little = 1;
+             $(".num").val(little);
+         } else {
+             $(".num").val(value);
+         }
+         console.log(value);
+     });
+
+
+     $(".add").on('click', function() {
+         var value = $(".num").val();
+         value++;
+         $(".num").val(value);
+         console.log(value);
+     });
+
+     //............................
+     //加入購物車
+     // 隱藏背景與訊息對話框
+     $("#bg").hide();
+
+     // 點按「Click Me!」按鈕
+     $(".btn_add_car").click(function() {
+         // 淡入顯示背景與訊息對話框
+         $("#bg").fadeIn(300);
+         $("#bg").delay(1000).fadeOut(300);
+     });
+
+     // 點按「OK」按鈕
+     //  $("#ok").click(function() {
+     //      // 淡出隱藏背景與訊息對話框
+     //      $("#bg").fadeOut(300);
+     //  });
+
+     //..................................................
+
+
+     // $(".sm_box a").click(function() {
+     //     $(".big_box img").attr("src", $(this).attr("href"));
+     //     return false;
+     // });
+
+
+
+
+     //.......................................
+
+
+
+     // // clone the text
+     // $(".zoomer:first").anythingZoomer({
+     //     clone: true
+     // });
+
+     // $(".zoomer:last").anythingZoomer();
+
+ });
