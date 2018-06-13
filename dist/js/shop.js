6b54a932e9b0ee42c66e52b77254d95a4c423062
Joanna Lin <okokis101@gmail.com>
2018-06-14 02:46:50 +0800

    complete shop hover animate

--- dist/js/shop.js
+++ dist/js/shop.js
@@ -39,5 +39,24 @@ $(function() {
         }
         return value;
     }
-
+    //...................
+    //加入購物車
+    $(".shop_add_car").click(function() {
+        // 淡入顯示背景與訊息對話框
+        $("#bg").fadeIn(300);
+        $("#bg").delay(1000).fadeOut(300);
+        return false;
+    });
+    //收藏
+    $(".shop_add_love").click(function() {
+        // 淡入顯示背景與訊息對話框
+        $("#bg2").fadeIn(300);
+        $("#bg2").delay(1000).fadeOut(300);
+        return false;
+    });
+    //.........................................
+    //按下產品跳轉產品頁
+    $('.box').click(function() {
+        window.location.href = '../html/buy_shop.html';
+    });
 })
