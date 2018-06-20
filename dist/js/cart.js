7d74072188b357b5f0d1050eb1ee8718eb2a1dc5
Joanna Lin <okokis101@gmail.com>
2018-06-20 19:44:19 +0800

    complete cashier & cart & shop & buy_shop & login & join

--- dist/js/cart.js
+++ dist/js/cart.js
@@ -1,23 +1,34 @@
 $(function() {
     //計數器加減數量
     $(".minus").on('click', function() {
-        var value = $(".num").val();
-        value--;
-        if (value < 2) {
-            var little = 1;
-            $(".num").val(little);
-        } else {
-            $(".num").val(value);
+        $(".current_minus").removeClass("current_minus");
+        $(this).addClass("current_minus");
+
+        if ($(this).hasClass('current_minus')) {
+            var value = $(".current_minus").next().val();
+            value--;
+            if (value < 2) {
+                var little = 1;
+                $(".current_minus").next().val(little);
+            } else {
+                $(".current_minus").next().val(value);
+            }
+            //console.log(value);
         }
-        console.log(value);
     });
 
 
     $(".add").on('click', function() {
-        var value = $(".num").val();
-        value++;
-        $(".num").val(value);
-        console.log(value);
+        $(".current_add").removeClass("current_add");
+        $(this).addClass("current_add");
+
+        if ($(this).hasClass('current_add')) {
+            //console.log($(".current").prev().val());
+            var value_add = $(".current_add").prev().val();
+            value_add++;
+            $(".current_add").prev().val(value_add);
+            //console.log(value_add);
+        };
     });
 
 });
